49ba2ca6bb20959c090b7ecb6abc5505d4baed07
Dániel Szilágyi <szilagyi.daniel88@gmail.com>
2025-07-15 14:51:17 +0200

    Update main.c

--- scheduler-pattern/main.c
+++ scheduler-pattern/main.c
@@ -5,11 +5,35 @@
 */
 
 #include <stdint.h>
+#include "task_scheduler.h"
 
 // Imaginary function that returns the elapsed milliseconds since boot
 uint32_t now_ms();
 
+// Some dummy functions can call
+void fn1(void);
+void fn2(void);
+void fn3(void);
+
 int main(void)
 {
+    // Create tasks
+    task_t* task1 = scheduler_create_task(fn1, 1, 1000, 0); // Runs once
+    task_t* task2 = scheduler_create_task(fn2, 1, 0, 500); // Runs periodically
+
+    // Add functions to scheduler
+    scheduler_add_task(task1);
+    scheduler_add_task(task2);
+
+    while(1)
+    {
+        // Run scheduler
+        uint32_t now = now_ms();
+        scheduler_run_tasks(now);
+
+        // Call another function
+        fn3(); // Runs every time
+    }
+
     return 0;
 }
